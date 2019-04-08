a1b69ddd221f0ad597ccadbe97fe512226e149fa
hieusmiths <hieusmiths@gmail.com>
2019-04-08 16:33:38 +0700

    Updates

---
+++ precache-manifest.fcd25737f299dd723f78cb56607b9850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "267b53bfcc35f3cf9d67",
+    "url": "/React-Router/static/js/runtime~main.358326bc.js"
+  },
+  {
+    "revision": "6cd91eb6ccb9e14319ee",
+    "url": "/React-Router/static/js/main.21615648.chunk.js"
+  },
+  {
+    "revision": "88d9541ca0e182356ec2",
+    "url": "/React-Router/static/js/2.fdd0085d.chunk.js"
+  },
+  {
+    "revision": "6cd91eb6ccb9e14319ee",
+    "url": "/React-Router/static/css/main.78db87ec.chunk.css"
+  },
+  {
+    "revision": "cf253e7af6147cb0fe86eb7d217527b3",
+    "url": "/React-Router/index.html"
+  }
+];
